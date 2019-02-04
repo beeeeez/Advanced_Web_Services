d932095dadccbd0453cf23809ee61c6d313d5b67
Brown <cibrown@labs.neit.edu>
2019-02-04 18:33:49 -0500

    boy howdy

--- lab3/server.js
+++ lab3/server.js
@@ -22,12 +22,46 @@ handlebars.registerHelper('tableGenerator', (poopy)=>{
     return jimmy;
 
 });
+
+handlebars.registerHelper('errurboi',()=>{
+
+    let num = floppy(1);
+    let plum = 0;
+    let jammy=""
+    let plammy="";
+
+    for(let i=0; i < num; i++){
+        plum = floppy(2);
+        if(plum==1){
+            plammy="still";
+        }
+        else if(plum==2){
+            plammy="rotate";
+        }
+        else if(plum==3){
+            plammy="shrink";
+        }
+        jammy+="<div class="+plammy+">Totally Boned</div>"
+    }
+   return jammy;
+});
  
 
 
  function balloons(){
     var color = ((1<<24)*Math.random()|0).toString(16);
     return color;
+ }
+
+ function floppy(a){
+     let bah;
+    if (a==1){
+         bah=Math.floor(Math.random() * 50) + 20
+    }
+    else if (a==2){
+        bah=Math.floor(Math.random() * 3) + 1
+    }
+    return bah;
  }
 
 
@@ -42,10 +76,13 @@ app.get("/",(req,res)=>{
     res.render('flubby.hbs');
 });
 
+app.get("/flubby",(req,res)=>{
+    res.render('flubby.hbs');
+});
 
 app.get("*", (req, res)=>{
 
-    res.render('flubby.hbs');
+    res.render('errur.hbs');
 });
 
 app.listen(3000, ()=>{
